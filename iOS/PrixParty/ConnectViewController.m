9723ad6748e14ec4ed5ab5d63cba4e951ebafca9
Rayne Beridon <rberidon@gmail.com>
2013-04-21 15:28:36 -0500

    Added Twitter API stuff

--- iOS/PrixParty/ConnectViewController.m
+++ iOS/PrixParty/ConnectViewController.m
@@ -31,6 +31,8 @@
     
 }
 
+@property (nonatomic) ACAccountStore *accountStore;
+
 @end
 
 @implementation ConnectViewController
@@ -38,6 +40,8 @@
 - (void)awakeFromNib
 {
     [super awakeFromNib];
+    
+    _accountStore = [[ACAccountStore alloc] init];
     
     tweetsRecentList = [[NSMutableArray alloc] init];
     tweetsTrendingList = [[NSMutableArray alloc] init];
@@ -88,7 +92,19 @@
     self.connectRecentTableView.hidden = YES;
     self.connectTrendingTableView.hidden = NO;
     
+    if ([self userHasAccessToTwitter]) {
+        NSLog(@"Has access to Twitter!");
+        [self fetchTimelineForUser:@"rberidon"];
+    } else {
+        NSLog(@"Does not have twitter");
+    }
     
+    if ([self userHasAccessToFacebook]) {
+        NSLog(@"Has access to Facebook");
+    } else {
+        NSLog(@"Does not have access to Facebook");
+    }
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -172,8 +188,6 @@
 }
 
 - (void)loadFirstData{
-    
-    
     
     NSURL *urlTrending = [NSURL URLWithString:trendingURL];
     NSURL *urlRecent = [NSURL URLWithString:recentURL];
@@ -290,6 +304,83 @@
     [operationRecent start];
     [operationTrending start];
     
+}
+
+- (BOOL)userHasAccessToTwitter
+{
+    return [SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter];
+}
+
+- (BOOL)userHasAccessToFacebook
+{
+    return [SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook];
+}
+
+- (void)fetchTimelineForUser:(NSString *)username
+{
+    //  Step 0: Check that the user has local Twitter accounts
+    if ([self userHasAccessToTwitter]) {
+        
+        //  Step 1:  Obtain access to the user's Twitter accounts
+        ACAccountType *twitterAccountType = [self.accountStore
+                                             accountTypeWithAccountTypeIdentifier:
+                                             ACAccountTypeIdentifierTwitter];
+        [self.accountStore
+         requestAccessToAccountsWithType:twitterAccountType
+         options:NULL
+         completion:^(BOOL granted, NSError *error) {
+             if (granted) {
+                 //  Step 2:  Create a request
+                 NSArray *twitterAccounts =
+                 [self.accountStore accountsWithAccountType:twitterAccountType];
+                 NSURL *url = [NSURL URLWithString:@"https://api.twitter.com"
+                               @"/1.1/statuses/user_timeline.json"];
+                 NSDictionary *params = @{@"screen_name" : username,
+                                          @"include_rts" : @"0",
+                                          @"trim_user" : @"1",
+                                          @"count" : @"20"};
+                 SLRequest *request =
+                 [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                    requestMethod:SLRequestMethodGET
+                                              URL:url
+                                       parameters:params];
+                 
+                 //  Attach an account to the request
+                 [request setAccount:[twitterAccounts lastObject]];
+                 
+                 //  Step 3:  Execute the request
+                 [request performRequestWithHandler:^(NSData *responseData,
+                                                      NSHTTPURLResponse *urlResponse,
+                                                      NSError *error) {
+                     if (responseData) {
+                         if (urlResponse.statusCode >= 200 && urlResponse.statusCode < 300) {
+                             NSError *jsonError;
+                             NSDictionary *timelineData =
+                             [NSJSONSerialization
+                              JSONObjectWithData:responseData
+                              options:NSJSONReadingAllowFragments error:&jsonError];
+                             
+                             if (timelineData) {
+                                 NSLog(@"Timeline Response: %@\n", timelineData);
+                             }
+                             else {
+                                 // Our JSON deserialization went awry
+                                 NSLog(@"JSON Error: %@", [jsonError localizedDescription]);
+                             }
+                         }
+                         else {
+                             // The server did not respond successfully... were we rate-limited?
+                             NSLog(@"The response status code is %d", urlResponse.statusCode);
+                         }
+                     }
+                 }];
+             }
+             else {
+                 // Access was not granted, or an error occurred
+                 NSLog(@"%@", [error localizedDescription]);
+             }
+         }];
+    }
 }
 
 - (Tweet *)retrieveTweetFromDataSource:(id) obj{
